6f8d091489983ed7818eaa598d625023c5f02237
AdityaP <blackholesarereal@gmail.com>
2022-06-26 23:57:18 +0530

    QuickSort

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Quicksort {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array and enter the elements: ");
+        System.out.println("Enter the size and then enter the elements of array: ");
         int n = sc.nextInt();
         int a[] = new int[n];
         for(int i = 0;i<n;i++){
@@ -15,6 +15,7 @@ public class Quicksort {
         }
 
     }
+    // Recursively calls the function for each half and so on
     private static void QuickSort(int []a,int l, int h) {
         if(l<h){
             int p = Partition(a,l,h);
@@ -22,6 +23,8 @@ public class Quicksort {
             QuickSort(a,p+1,h);
         }
     }
+    // Main Sorting Function takes first element as pivot element and places it in the right position and
+    // return that position for every half iterative step
     private static int Partition(int a[],int l, int h) {
         int p = a[l];
         int i =l;
@@ -38,6 +41,7 @@ public class Quicksort {
         }
         return j;
     }
+    // swaps the elements
     static void swap(int a[], int p, int q){
         int t = a[p];
         a[p] = a[q];
